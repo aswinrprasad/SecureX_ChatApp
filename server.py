f3f823758e451c38aef9e05d9754de26a091eecc
aswinrprasad <aswinrprasad@gmail.com>
2018-10-26 23:42:59 +0530

    Server error debug

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def accept_incoming_connections():
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave!"+"Now type your name and press enter!", "utf8"))
+        client.send(bytes("Greetings from the cave!"+"Now type your name and press enter!"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
@@ -23,25 +23,25 @@ def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
     name = client.recv(BUFSIZ).decode("utf8")
     welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
+    client.send(bytes(welcome))
     msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
+    broadcast(bytes(msg))
     clients[client] = name
     while True:
         msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
+        if msg != bytes("{quit}"):
             broadcast(msg, name+": ")
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes("{quit}"))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            broadcast(bytes("%s has left the chat." % name))
             break
 
 def broadcast(msg, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+        sock.send(bytes(prefix)+msg)
 
 if __name__ == "__main__":
     SERVER.listen(5)  # Listens for 5 connections at max.
